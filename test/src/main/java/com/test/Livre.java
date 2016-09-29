28e199cd9536fb1619089d6a23681e4ea5e631f0
Bilel <Bilel@Bile-PC>
2016-09-29 12:40:50 +0100

    deux classe
    Signed-off-by: Bilel <Bilel@Bile-PC>

---
+++ test/src/main/java/com/test/Livre.java
@@ -0,0 +1,27 @@
+package com.test;
+
+public class Livre {
+	String titre;
+	String isbn;
+	String datesortie;
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getDatesortie() {
+		return datesortie;
+	}
+	public void setDatesortie(String datesortie) {
+		this.datesortie = datesortie;
+	}
+		
+
+}
